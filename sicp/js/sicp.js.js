d40233a4478166713122b2ba913ae1709dc2c2b6
lazinessisavirtue <lazinessisavirtue@gmail.com>
2015-02-19 23:13:06 -0700

    add scoping

--- sicp/js/sicp.js.js
+++ sicp/js/sicp.js.js
@@ -1,48 +1,53 @@
 (function (window) {
 	
-	util = {
-		arrayOf: function (a) {
-			return Array.prototype.slice.apply(a);
-		}
-	};
-	
-	var sicp = function () {
-		if (arguments.length == 0) {
-			return sicp.list();
-		} else if (arguments.length == 1 && typeof arguments[0] == 'string') {
-			return sicp.get.apply(this, util.arrayOf(arguments));
-		} else if (arguments.length == 2 && typeof arguments[0] == 'string'
-			&& typeof arguments[1] == 'function') {
-			return sicp.build.apply(this, arguments);
-		} else {
-			console.log(arguments);
-			throw "unrecognizable arguments";
-		}
-	};
-	
-	sicp.map = {};
+	function sicpFactory(scope) {
+		var sicp = function () {
+			if (arguments.length == 0) {
+				return sicp.list();
+			} else if (arguments.length == 1 && typeof arguments[0] == 'string') {
+				return sicp.get.apply(this, arguments);
+			} else if (arguments.length == 2 && typeof arguments[0] == 'string'
+				&& typeof arguments[1] == 'function') {
+				return sicp.define.apply(this, arguments);
+			} else {
+				console.log(arguments);
+				throw "unrecognizable arguments";
+			}
+		};
+		
+		sicp.map = Object.create(scope);
 
-	sicp.list = function () {
-		var l = [];
-		for (var k in sicp.map) {
-			l.push(k);
-		}
-		return l;
-	};
+		sicp.list = function () {
+			var l = [];
+			for (var k in sicp.map) {
+				l.push(k);
+			}
+			return l;
+		};
 
-	sicp.build = function (key, procedure) {
-		var entry = function () {
-			return procedure.apply(this, arguments);
-		}
-		entry.key = key;
-		sicp.map[key] = entry;
-		return entry;
-	};
+		sicp.define = function (key, procedure) {
+			var entry = function () {
+				return procedure.apply(entry.sicp, arguments);
+			}
+			entry.key = key;
+			entry.sicp = sicpFactory(sicp.map);
+			sicp.map[key] = entry;
+			return entry;
+		};
 
-	sicp.get = function (key) {
-		return sicp.map[key];
-	};
+		sicp.get = function (key) {
+			return sicp.map[key];
+		};
+		
+		return sicp;
+	}
 	
-	window.sicp = window.sicp_js = sicp;
+	window.sicp_js = sicpFactory(null);
+	if (window.sicp) {
+		window.sicp.js = window.sicp_js;
+	} else {
+		window.sicp = window.sicp_js;
+		window.sicp.js = window.sicp_js;
+	}
 	
 } (typeof window !== "undefined" ? window : this));
