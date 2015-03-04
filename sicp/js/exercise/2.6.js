0d27bd6ec70b3bc79d40a1fb4073ede128c540f0
lazinessisavirtue <lazinessisavirtue@gmail.com>
2015-03-04 00:16:49 -0700

    2.6

---
+++ sicp/js/exercise/2.6.js
@@ -0,0 +1,38 @@
+(function (sicp) {
+	
+	sicp.zero = function (f) {
+		return function (x) {
+			return x;
+		};
+	};
+	
+	sicp.add1 = function (n) {
+		return function (f) {
+			return function (x) {
+				return f(n(f)(x));
+			};
+		};
+	};
+
+	sicp.addToN = function (n) {
+		return (n === 0) ? sicp.zero : sicp.add1(sicp.addToN(n - 1));
+	};
+
+	return sicp.addToN(10)(function (x) { return x * 2; })(3);
+	
+})(sicp.$);
+
+/*
+	Proof by Induction: addToN(n)(f)(x) = f^n(x)
+	
+	Base case: addToN(0)(f)(x) = zero = f^0(x)
+	
+	Inductive Step:
+		Assume: addToN(n)(f)(x) = f^n(x)
+		Then: addToN(n + 1)(f)(x)
+			= add1(addToN(n))(f)(x)
+			= f(addToN(n)(f)(x))
+			= f(f^n(x))
+			= f^(n + 1)(x)
+*/
+
